daae58bf759452070d117e39db7b5258f88cd405
Yuri Petrik <yuri.petrik@gmail.com>
2020-11-13 04:51:35 +0200

    1111

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5502');
-  res.setHeader('Access-Control-Allow-Origin', 'https://bratan.ooo');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://bratan.ooo');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
