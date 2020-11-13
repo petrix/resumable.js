3440bf313dde0f159c07d3bdb97fe0b7ba30f185
Yuri Petrik <yuri.petrik@gmail.com>
2020-11-13 04:48:41 +0200

    zzz

--- app.js
+++ app.js
@@ -10,11 +10,28 @@ var crypto = require('crypto');
 // var http = require('http');
 var http = require('http').createServer(app);
 
-// var io = require('socket.io')(http);
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-// });
-// Host most stuff in the public folder
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5502');
+  res.setHeader('Access-Control-Allow-Origin', 'https://bratan.ooo');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/jquery-ui', express.static(__dirname + '/node_modules/jquery-ui-dist'));
 app.use('/video-js', express.static(__dirname + '/node_modules/video.js/dist'));
