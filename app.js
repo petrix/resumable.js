f4f33040deec59424a1347dd51ef3f1b8b68c52c
Yuri Petrik <yuri.petrik@gmail.com>
2020-11-15 15:43:53 +0200

    cors worked

--- app.js
+++ app.js
@@ -42,51 +42,50 @@ var httpsOptions = {
 //   next();
 // });
 
-var allowlist = ['https://o.bratan.ooo', 'https://bratan.ooo','http://127.0.0.1:5502', 'https://p3xx.tk']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-// fileid?filename=
-app.get('/', cors(corsOptionsDelegate), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for an allowed domain.'})
-})
-app.post('/upload', cors(corsOptionsDelegate), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for an allowed domain.'})
-});
+// var allowlist = ['https://o.bratan.ooo', 'https://bratan.ooo','http://127.0.0.1:5502', 'https://p3xx.tk']
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
+// app.get('/', cors(corsOptionsDelegate), function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for an allowed domain.'})
+// })
+// app.post('/upload', cors(corsOptionsDelegate), function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for an allowed domain.'})
+// });
 
-
+app.use(cors());
 
 
 var secureServer = https.createServer(httpsOptions, app).listen(httpsPORT, function () {
   console.log('HTTPS Server Listener Started:', httpsPORT);
 });
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://o.bratan.ooo');
-  // res.setHeader('Access-Control-Allow-Origin', 'https://bratan.ooo');
+//   // Website you wish to allow to connect
+//   // res.setHeader('Access-Control-Allow-Origin', 'https://o.bratan.ooo');
+//   // res.setHeader('Access-Control-Allow-Origin', 'https://bratan.ooo');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 // app.use(cors());
 
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
