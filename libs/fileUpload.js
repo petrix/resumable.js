1e3506c790b8672b7f65684f57b412955c75bae9
p3xx <yuri.petrik@gmail.com>
2021-04-05 16:43:42 +0300

    czxczxczxczxczxc

--- libs/fileUpload.js
+++ libs/fileUpload.js
@@ -1,239 +1,229 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 // const ffmpeg = require('ffmpeg');
 
-var ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-var ffmpeg = require('fluent-ffmpeg');
-var ffmpegOnProgress = require('ffmpeg-on-progress');
-ffmpeg.setFfmpegPath(ffmpegPath);
-ffmpeg.setFfprobePath(ffmpegPath);
-var command = ffmpeg();
+// var ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+// var ffmpeg = require('fluent-ffmpeg');
+// var ffmpegOnProgress = require('ffmpeg-on-progress');
+// ffmpeg.setFfmpegPath(ffmpegPath);
+// ffmpeg.setFfprobePath(ffmpegPath);
+// var command = ffmpeg();
 const logProgress = (progress, event) => {
-    // progress is a floating point number from 0 to 1
-    console.log('progress', (progress * 100).toFixed(2));
-}
-  const executeFfmpeg = args => {
-    let command = ffmpeg().output(' '); // pass "Invalid output" validation
-    command._outputs[0].isFile = false; // disable adding "-y" argument
-    command._outputs[0].target = ""; // bypass "Unable to find a suitable output format for ' '"
-    command._global.get = () => { // append custom arguments
-        return typeof args === "string" ? args.split(' ') : args;
-    };
-    return command;
+	// progress is a floating point number from 0 to 1
+	console.log("progress", (progress * 100).toFixed(2));
+};
+const executeFfmpeg = args => {
+	let command = ffmpeg().output(" "); // pass "Invalid output" validation
+	command._outputs[0].isFile = false; // disable adding "-y" argument
+	command._outputs[0].target = ""; // bypass "Unable to find a suitable output format for ' '"
+	command._global.get = () => {
+		// append custom arguments
+		return typeof args === "string" ? args.split(" ") : args;
+	};
+	return command;
 };
 
-
-
 exports.getFullPath = function (_path) {
-
-    _path = [path.dirname(require.main.filename)].concat(_path);
-    return (path.join.apply(null, _path));
+	_path = [path.dirname(require.main.filename)].concat(_path);
+	return path.join.apply(null, _path);
 };
 
 exports.makeDirectory = function (dirName) {
-
-    fs.mkdirSync(dirName, {
-        recursive: true
-    });
+	fs.mkdirSync(dirName, {
+		recursive: true,
+	});
 };
 
 exports.saveFile = function (fileToUpload, filePath) {
-
-    const fileBuffer = fs.readFileSync(fileToUpload.path);
-    fs.writeFileSync(filePath, fileBuffer);
-    return filePath;
+	const fileBuffer = fs.readFileSync(fileToUpload.path);
+	fs.writeFileSync(filePath, fileBuffer);
+	return filePath;
 };
 ////////////////
 
 exports.readFileNames = function (filePath) {
-    const readDir = fs.readdirSync(filePath, (err, files) => {});
-    var $request = [];
+	const readDir = fs.readdirSync(filePath, (err, files) => {});
+	var $request = [];
 
-    readDir.forEach(function (file) {
-        var size = fs.statSync(filePath + '/' + file).size;
-        $request.push(file + '#' + size);
-        // console.log(file, size);
-        // return file, size;
-    });
-    return $request;
+	readDir.forEach(function (file) {
+		var size = fs.statSync(filePath + "/" + file).size;
+		$request.push(file + "#" + size);
+		// console.log(file, size);
+		// return file, size;
+	});
+	return $request;
 
-
-    //  console.log(readDir);
-    //  return readDir;
-    // const files = fs.readdirSync(filePath);
-    // for(let file of files){
-    // }
-    // return files;
+	//  console.log(readDir);
+	//  return readDir;
+	// const files = fs.readdirSync(filePath);
+	// for(let file of files){
+	// }
+	// return files;
 };
 exports.removeFile = function (fileName, filePath) {
-    const files = fs.readdirSync(filePath);
-    const convFiles = fs.readdirSync(`${filePath}-converted`);
-    for (let file of files) {
-        if (file === fileName) {
-            fs.unlinkSync(path.join(filePath, file));
-        }
-    }
-    for (let convFile of convFiles){
-        var x = convFile.split('-ixi-')[0];
-        if(x == fileName){
-                    console.log(x);
-     
-                        fs.unlinkSync(path.join(`${filePath}-converted`, convFile));
+	const files = fs.readdirSync(filePath);
+	const convFiles = fs.readdirSync(`${filePath}-converted`);
+	for (let file of files) {
+		if (file === fileName) {
+			fs.unlinkSync(path.join(filePath, file));
+		}
+	}
+	for (let convFile of convFiles) {
+		var x = convFile.split("-ixi-")[0];
+		if (x == fileName) {
+			console.log(x);
 
-        }
-    }
-    return files;
+			fs.unlinkSync(path.join(`${filePath}-converted`, convFile));
+		}
+	}
+	return files;
 };
 var convertingItems = [];
 var convertingCount = 0;
-exports.convert1080 = function (fileName, filePath) {
-    const files = fs.readdirSync(filePath);
-    var newFileName;
-    for (let file of files) {
-        if (file === fileName) {
+// exports.convert1080 = function (fileName, filePath) {
+// 	const files = fs.readdirSync(filePath);
+// 	var newFileName;
+// 	for (let file of files) {
+// 		if (file === fileName) {
+// 			var fileDst = file.split(".");
+// 			console.log(fileDst);
 
-            var fileDst = file.split('.');
-            console.log(fileDst);
+// 			// for(var iii=0;i<fileDst.length-1;iii++){
+// 			//     fileDstName+=fileDst[iii]+'.';
+// 			// }
+// 			newFileName = file + "-ixi-1080p" + ".mp4";
+// 			var command = ffmpeg()
+// 				.input(filePath + "/" + file)
+// 				.output(filePath + "-converted/" + newFileName)
+// 				.videoCodec("libx264")
+// 				// .inputOptions('-preset fast')
+// 				.size("1920x?")
+// 				.audioCodec("aac")
+// 				.keepDAR()
+// 				// .on('progress', ffmpegOnProgress(logProgress))
+// 				.on("progress", cmd => console.log("cmd", cmd))
+// 				.on("start", commandLine => console.log("start", commandLine))
+// 				// .on('codecData', codecData => console.log('codecData', codecData))
+// 				.on("error", error => console.log("error", error))
+// 				// .on('stderr', stderr => console.log('stderr', stderr))
+// 				.on("end", end => console.log("end", end))
+// 				.run();
+// 		}
+// 	}
+// 	return newFileName;
+// };
+// exports.convert720 = function (fileName, filePath) {
+// 	const files = fs.readdirSync(filePath);
+// 	var newFileName;
+// 	for (let file of files) {
+// 		if (file === fileName) {
+// 			var fileDst = file.split(".");
+// 			console.log(fileDst);
 
-            // for(var iii=0;i<fileDst.length-1;iii++){
-            //     fileDstName+=fileDst[iii]+'.';
-            // }
-            newFileName =file + '-ixi-1080p' + '.mp4';
-            var command = ffmpeg().input(filePath + '/' + file)
-                .output(filePath + '-converted/'+ newFileName)
-                .videoCodec('libx264')
-                // .inputOptions('-preset fast')
-                .size('1920x?')
-                .audioCodec('aac')
-                .keepDAR()
-                // .on('progress', ffmpegOnProgress(logProgress))
-                .on('progress', cmd => console.log('cmd',cmd))
-                .on('start', commandLine => console.log('start', commandLine))
-                // .on('codecData', codecData => console.log('codecData', codecData))
-                .on('error', error => console.log('error', error))
-                // .on('stderr', stderr => console.log('stderr', stderr))
-                .on('end', end => console.log('end', end))
-                .run();
+// 			// for(var iii=0;i<fileDst.length-1;iii++){
+// 			//     fileDstName+=fileDst[iii]+'.';
+// 			// }
+// 			newFileName = file + "-ixi-720p" + ".mp4";
+// 			var probe = ffmpeg()
+// 				.input(filePath + "/" + file)
+// 				.ffprobe((err, data) => {
+// 					console.log(data);
+// 				});
+// 			var command = ffmpeg()
+// 				.input(filePath + "/" + file)
+// 				// .ffprobe(function(err,data){console.log(data)})
+// 				.output(filePath + "-converted/" + newFileName)
+// 				.videoCodec("libx264")
+// 				// .inputOptions('-preset fast')
+// 				.size("1280x?")
+// 				.audioCodec("aac")
+// 				.keepDAR()
+// 				// .on('progress', ffmpegOnProgress(logProgress))
+// 				.on("progress", cmd => console.log("cmd", cmd))
+// 				.on("start", commandLine => console.log("start", commandLine))
+// 				.on("codecData", function (data) {
+// 					console.log("Input is " + data.audio + " audio " + "with " + data.video + " video");
+// 				})
+// 				// .on('codecData', codecData => console.log('codecData', codecData))
+// 				.on("error", error => console.log("error", error))
+// 				// .on('stderr', stderr => console.log('stderr', stderr))
+// 				.on("end", end => console.log("end", end))
+// 				.run();
+// 		}
+// 	}
+// 	return newFileName;
+// };
+// exports.convert360 = function (fileName, filePath) {
+// 	console.log("FU360");
+// 	var newFileName;
+// 	const files = fs.readdirSync(filePath);
+// 	for (let file of files) {
+// 		if (file === fileName) {
+// 			var fileDst = file.split(".");
+// 			console.log(fileDst);
 
-        }
-    }
-    return newFileName;
-};
-exports.convert720 = function (fileName, filePath) {
-    const files = fs.readdirSync(filePath);
-    var newFileName;
-    for (let file of files) {
-        if (file === fileName) {
-
-            var fileDst = file.split('.');
-            console.log(fileDst);
-
-            // for(var iii=0;i<fileDst.length-1;iii++){
-            //     fileDstName+=fileDst[iii]+'.';
-            // }
-            newFileName =file + '-ixi-720p' + '.mp4';
-            var probe = ffmpeg().input(filePath+'/'+file).ffprobe((err,data)=>{console.log(data)})
-            var command = ffmpeg().input(filePath + '/' + file)
-                // .ffprobe(function(err,data){console.log(data)})
-                .output(filePath + '-converted/'+ newFileName)
-                .videoCodec('libx264')
-                // .inputOptions('-preset fast')
-                .size('1280x?')
-                .audioCodec('aac')
-                .keepDAR()
-                // .on('progress', ffmpegOnProgress(logProgress))
-                .on('progress', cmd => console.log('cmd',cmd))
-                .on('start', commandLine => console.log('start', commandLine))
-                .on('codecData', function(data) {
-                    console.log('Input is ' + data.audio + ' audio ' +
-                      'with ' + data.video + ' video');
-                  })
-                // .on('codecData', codecData => console.log('codecData', codecData))
-                .on('error', error => console.log('error', error))
-                // .on('stderr', stderr => console.log('stderr', stderr))
-                .on('end', end => console.log('end', end))
-                .run();
-
-        }
-    }
-    return newFileName;
-};
-exports.convert360 = function (fileName, filePath) {
-    console.log('FU360')
-    var newFileName;
-    const files = fs.readdirSync(filePath);
-    for (let file of files) {
-        if (file === fileName) {
-
-            var fileDst = file.split('.');
-            console.log(fileDst);
-
-            // for(var iii=0;i<fileDst.length-1;iii++){
-            //     fileDstName+=fileDst[iii]+'.';
-            // }
-            newFileName =file + '-ixi-360p' + '.mp4';
-            var command = ffmpeg().input(filePath + '/' + file)
-                .output(filePath + '-converted/'+ newFileName)
-                .videoCodec('libx264')
-                // .inputOptions('-preset fast')
-                .size('640x?')
-                .audioCodec('aac')
-                .keepDAR()
-                // .on('progress', ffmpegOnProgress(logProgress))
-                .on('progress', cmd => console.log('cmd',cmd))
-                .on('start', commandLine => console.log('start', commandLine))
-                // .on('codecData', codecData => console.log('codecData', codecData))
-                .on('error', error => console.log('error', error))
-                // .on('stderr', stderr => console.log('stderr', stderr))
-                .on('end', end => console.log('end', end))
-                .run();
-
-        }
-    }
-    return files;
-};
+// 			// for(var iii=0;i<fileDst.length-1;iii++){
+// 			//     fileDstName+=fileDst[iii]+'.';
+// 			// }
+// 			newFileName = file + "-ixi-360p" + ".mp4";
+// 			var command = ffmpeg()
+// 				.input(filePath + "/" + file)
+// 				.output(filePath + "-converted/" + newFileName)
+// 				.videoCodec("libx264")
+// 				// .inputOptions('-preset fast')
+// 				.size("640x?")
+// 				.audioCodec("aac")
+// 				.keepDAR()
+// 				// .on('progress', ffmpegOnProgress(logProgress))
+// 				.on("progress", cmd => console.log("cmd", cmd))
+// 				.on("start", commandLine => console.log("start", commandLine))
+// 				// .on('codecData', codecData => console.log('codecData', codecData))
+// 				.on("error", error => console.log("error", error))
+// 				// .on('stderr', stderr => console.log('stderr', stderr))
+// 				.on("end", end => console.log("end", end))
+// 				.run();
+// 		}
+// 	}
+// 	return files;
+// };
 ////////////////
 exports.clearFolder = function (dirName, filterArray = []) {
-
-    const files = fs.readdirSync(dirName);
-    for (let file of files) {
-
-        let deleteFile = false;
-        if (filterArray && filterArray.length > 0) {
-            filterArray.some(filter => {
-                if (filter.test(file)) {
-                    deleteFile = true;
-                    return true;
-                }
-            })
-        } else {
-            deleteFile = true;
-        }
-        if (deleteFile) {
-            fs.unlinkSync(path.join(dirName, file));
-        }
-    }
+	const files = fs.readdirSync(dirName);
+	for (let file of files) {
+		let deleteFile = false;
+		if (filterArray && filterArray.length > 0) {
+			filterArray.some(filter => {
+				if (filter.test(file)) {
+					deleteFile = true;
+					return true;
+				}
+			});
+		} else {
+			deleteFile = true;
+		}
+		if (deleteFile) {
+			fs.unlinkSync(path.join(dirName, file));
+		}
+	}
 };
 
-
 function getFileSize(filePath, file) {
-    fs.open(filePath + "/" + file, "r", (err, fd) => {
-        if (err) throw err;
-        fs.fstat(fd, (err, stat) => {
-            if (err) throw err;
-            //   console.log(fd);
-            var size = stat.size;
-            var b = {
-                file: file,
-                size: size
-            };
+	fs.open(filePath + "/" + file, "r", (err, fd) => {
+		if (err) throw err;
+		fs.fstat(fd, (err, stat) => {
+			if (err) throw err;
+			//   console.log(fd);
+			var size = stat.size;
+			var b = {
+				file: file,
+				size: size,
+			};
 
-            fs.close(fd, err => {
-                if (err) throw err;
-            });
-            console.log(b);
-            return b;
-        });
-
-    });
+			fs.close(fd, err => {
+				if (err) throw err;
+			});
+			console.log(b);
+			return b;
+		});
+	});
 }
--- routes/upload.js
+++ routes/upload.js
@@ -1,16 +1,14 @@
-const fs = require('fs');
-const Config = require('../libs/config');
-const fileUpload = require('../libs/fileUpload');
+const fs = require("fs");
+const Config = require("../libs/config");
+const fileUpload = require("../libs/fileUpload");
 const upDir = fileUpload.getFullPath([Config.dirs.publicDir, Config.dirs.uploadDir]);
-const resumable = require('../libs/resumableNode')(upDir);
+const resumable = require("../libs/resumableNode")(upDir);
 const checkFolder = fileUpload.getFullPath([Config.dirs.filesDir]);
 const convertedFolder = fileUpload.getFullPath([Config.dirs.convertedDir]);
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 function cryptoId(name) {
-    return crypto.createHash('sha256', Config.brand)
-        .update(name)
-        .digest('hex')
+	return crypto.createHash("sha256", Config.brand).update(name).digest("hex");
 }
 // var io;
 // var date
@@ -30,147 +28,139 @@ function cryptoId(name) {
 //     })
 //     });
 
-
 // }
 exports.get = function (req, res) {
+	switch (req.url.split("?")[0]) {
+		case "/convert1080p":
+			console.log("/convert1080p", req.query.filename);
+			var fileId = cryptoId(req.query.filename);
+			try {
+				fileUpload.convert1080(req.query.filename, checkFolder, function (result) {
+					res.send(result);
+				});
+			} catch (err) {
+				console.error(err);
+			}
+			break;
+		case "/convert720p":
+			console.log("/convert720p", req.query.filename);
+			// res.send(fileUpload.readFile(checkFolder));
+			var fileId = cryptoId(req.query.filename);
+			try {
+				fileUpload.convert720(req.query.filename, checkFolder, function (result) {
+					res.send(result);
+				});
+			} catch (err) {
+				console.error(err);
+			}
+			break;
+		case "/convert360p":
+			console.log("/convert360p", req.query.filename);
+			// res.send(fileUpload.readFile(checkFolder));
+			var fileId = cryptoId(req.query.filename);
+			try {
+				fileUpload.convert360(req.query.filename, checkFolder);
+				res.send(fileId);
+			} catch (err) {
+				console.error(err);
+			}
+			break;
+		case "/rmFile":
+			console.log("/rmFile", req.query.filename);
+			// res.send(fileUpload.readFile(checkFolder));
+			var fileId = cryptoId(req.query.filename);
+			try {
+				fileUpload.removeFile(req.query.filename, checkFolder);
+				res.send(fileId);
+			} catch (err) {
+				console.error(err);
+			}
+			break;
+		case "/getfn":
+			var getfn = [];
+			fileUpload.readFileNames(checkFolder).forEach(function (fileString) {
+				namArray = fileString.split("#");
+				getfn.push(namArray[0] + "#" + namArray[1] + "#" + cryptoId(namArray[0]));
+			});
+			res.send(getfn);
+			break;
+		case "/getConverted":
+			var getfn = [];
+			fileUpload.readFileNames(convertedFolder).forEach(function (fileString) {
+				namArray = fileString.split("#");
+				getfn.push(namArray[0] + "#" + namArray[1] + "#" + cryptoId(namArray[0]));
+			});
+			res.send(getfn);
+			break;
+		case "/upload":
+			// Handle status checks on chunks through Resumable.js
+			resumable.get(req, function (status, filename, original_filename, identifier) {
+				// res.send((status === 'found' ? 200 : 404), status);
+				// res.send((status === 'found' ? 200 : 300), status);
+				res.status(status).sendStatus(status === "found" ? 200 : 300);
+			});
+			break;
+		case "/fileid":
+			// retrieve file id. invoke with /fileid?filename=my-file.jpg
+			fileUpload.readFileNames(checkFolder).forEach(function (fileString) {
+				if (fileString.split("#")[0] === req.query.filename) {
+					// console.log(fileString);
+				} else {
+					// return;
+					// res.sendStatus(404);
+				}
+			});
 
-    switch (req.url.split('?')[0]) {
-        case '/convert1080p':
-            console.log('/convert1080p', req.query.filename);
-            // res.send(fileUpload.readFile(checkFolder));
-            var fileId = cryptoId(req.query.filename);
-            try {
-                fileUpload.convert1080(req.query.filename, checkFolder, function (result) {
-                    res.send(result);
-                });
-
-            } catch (err) {
-                console.error(err);
-            }
-            break;
-        case '/convert720p':
-            console.log('/convert720p', req.query.filename);
-            // res.send(fileUpload.readFile(checkFolder));
-            var fileId = cryptoId(req.query.filename);
-            try {
-                fileUpload.convert720(req.query.filename, checkFolder, function (result) {
-                    res.send(result);
-                });
-
-            } catch (err) {
-                console.error(err);
-            }
-            break;
-        case '/convert360p':
-            console.log('/convert360p', req.query.filename);
-            // res.send(fileUpload.readFile(checkFolder));
-            var fileId = cryptoId(req.query.filename);
-            try {
-                fileUpload.convert360(req.query.filename, checkFolder);
-                res.send(fileId);
-            } catch (err) {
-                console.error(err);
-            }
-            break;
-        case '/rmFile':
-            console.log('/rmFile', req.query.filename);
-            // res.send(fileUpload.readFile(checkFolder));
-            var fileId = cryptoId(req.query.filename);
-            try {
-                fileUpload.removeFile(req.query.filename, checkFolder);
-                res.send(fileId);
-            } catch (err) {
-                console.error(err);
-            }
-            break;
-        case '/getfn':
-            var getfn = [];
-            fileUpload.readFileNames(checkFolder).forEach(function (fileString) {
-                namArray = fileString.split('#');
-                getfn.push(namArray[0] + '#' + namArray[1] + '#' + cryptoId(namArray[0]));
-            });
-            res.send(getfn);
-            break;
-        case '/getConverted':
-            var getfn = [];
-            fileUpload.readFileNames(convertedFolder).forEach(function (fileString) {
-                namArray = fileString.split('#');
-                getfn.push(namArray[0] + '#' + namArray[1] + '#' + cryptoId(namArray[0]));
-            });
-            res.send(getfn);
-            break;
-        case '/upload':
-            // Handle status checks on chunks through Resumable.js
-            resumable.get(req, function (status, filename, original_filename, identifier) {
-                // res.send((status === 'found' ? 200 : 404), status);
-                // res.send((status === 'found' ? 200 : 300), status);
-                res.status(status).sendStatus(status === 'found' ? 200 : 300);
-            });
-            break;
-        case '/fileid':
-            // retrieve file id. invoke with /fileid?filename=my-file.jpg
-            fileUpload.readFileNames(checkFolder).forEach(function (fileString) {
-                if (fileString.split('#')[0] === req.query.filename) {
-                    // console.log(fileString);
-                } else {
-                    // return;
-                    // res.sendStatus(404);
-                }
-            });
-
-
-
-            if (!req.query.filename) {
-                return res.status(500).end('query parameter missing');
-            }
-            // create md5 hash from filename
-            res.end(
-                cryptoId(req.query.filename)
-                // crypto.createHash('sha256', Config.brand)
-                // .update(req.query.filename)
-                // .digest('hex')
-            );
-            break;
-    }
+			if (!req.query.filename) {
+				return res.status(500).end("query parameter missing");
+			}
+			// create md5 hash from filename
+			res.end(
+				cryptoId(req.query.filename),
+				// crypto.createHash('sha256', Config.brand)
+				// .update(req.query.filename)
+				// .digest('hex')
+			);
+			break;
+	}
 };
 
 exports.post = function (req, res) {
-    // console.log('post',req.body.action)
-    // console.log(fileUpload.getFullPath([Config.dirs.publicDir, Config.dirs.uploadDir]));
-    if (req.body.action === 'cancelUpload') {
-        console.log('cancelUpload')
-        let filterArray = req.body.nowUploading ? req.body.nowUploading.split(',') : [];
-        filterArray = filterArray.map(upl => {
-            return new RegExp(`^res-${upl}.*`);
-        });
-        // console.log(filterArray);
+	// console.log('post',req.body.action)
+	// console.log(fileUpload.getFullPath([Config.dirs.publicDir, Config.dirs.uploadDir]));
+	if (req.body.action === "cancelUpload") {
+		console.log("cancelUpload");
+		let filterArray = req.body.nowUploading ? req.body.nowUploading.split(",") : [];
+		filterArray = filterArray.map(upl => {
+			return new RegExp(`^res-${upl}.*`);
+		});
+		// console.log(filterArray);
 
-        fileUpload.clearFolder(fileUpload.getFullPath([Config.dirs.publicDir, Config.dirs.uploadDir]), filterArray);
+		fileUpload.clearFolder(fileUpload.getFullPath([Config.dirs.publicDir, Config.dirs.uploadDir]), filterArray);
+	} else {
+		resumable.post(req, function (status, filename, original_filename, identifier) {
+			console.log("POST", status, original_filename, identifier);
+			if (status === "done") {
+				//when all chunks uploaded, then createWriteStream to /files folder with filename
+				const stream = fs.createWriteStream(fileUpload.getFullPath([Config.dirs.filesDir, filename]));
 
-    } else {
-        resumable.post(req, function (status, filename, original_filename, identifier) {
-            console.log('POST', status, original_filename, identifier);
-            if (status === 'done') {
-                //when all chunks uploaded, then createWriteStream to /files folder with filename
-                const stream = fs.createWriteStream(fileUpload.getFullPath([Config.dirs.filesDir, filename]));
-
-                const onDone = function (identifier) {
-                    // console.log(stream);
-                    resumable.clean(identifier);
-                };
-                //stitches the file chunks back together to create the original file.
-                resumable.write(identifier, stream, {
-                    onDone: onDone
-                });
-            }
-            try {
-                if (status == 'done') {
-                    console.log(status);
-                }
-                res.send(status);
-            } catch (err) {
-                console.error(err);
-            }
-        });
-    }
+				const onDone = function (identifier) {
+					// console.log(stream);
+					resumable.clean(identifier);
+				};
+				//stitches the file chunks back together to create the original file.
+				resumable.write(identifier, stream, {
+					onDone: onDone,
+				});
+			}
+			try {
+				if (status == "done") {
+					console.log(status);
+				}
+				res.send(status);
+			} catch (err) {
+				console.error(err);
+			}
+		});
+	}
 };
